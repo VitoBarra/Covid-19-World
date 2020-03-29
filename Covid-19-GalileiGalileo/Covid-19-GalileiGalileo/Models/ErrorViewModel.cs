1b44e32bbbba90bb7df2604701abdedda03344c7
Vito Barra <vb.vitobarra@gmail.com>
2020-03-29 16:05:38 +0200

    commit start

---
+++ Covid-19-GalileiGalileo/Covid-19-GalileiGalileo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Covid_19_GalileiGalileo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
