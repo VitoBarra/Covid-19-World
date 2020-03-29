2fd373e6f8ff4a3010bfd77052cced10fa6ecd0c
Vito Barra <vb.vitobarra@gmail.com>
2020-03-29 16:05:38 +0200

    commit start add live Reload

---
+++ Covid-19-GalileiGalileo/Covid-19-GalileiGalileo/Services/Data.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid_19_GalileiGalileo.Services
+{
+    
+    public class CovidDataData
+    {
+
+
+        [JsonProperty("total_deaths")]
+        public string total_deaths { get; set; }
+        [JsonProperty("total_cases")]
+        public string total_cases { get; set; }
+        [JsonProperty("total_recovered")]
+        public string total_recovered { get; set; }
+        [JsonProperty("new_cases")]
+        public string new_cases { get; set; }
+        [JsonProperty("new_deaths")]
+        public string new_deaths { get; set; }
+        [JsonProperty("statistic_taken_at")]
+        DateTime statistic_taken_at { get; set; }
+    }
+}
