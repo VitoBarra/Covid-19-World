abcff0ad0642448e352c7019af3896cf3f6b12c3
Vito Barra <vb.vitobarra@gmail.com>
2020-03-30 06:06:53 +0200

    service

--- Covid-19-GalileiGalileo/Covid-19-GalileiGalileo/Program.cs
+++ Covid-19-GalileiGalileo/Covid-19-GalileiGalileo/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Covid_19_GalileiGalileo.Seriveces;
+using Covid_19_GalileiGalileo.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -11,10 +13,19 @@ namespace Covid_19_GalileiGalileo
 {
     public class Program
     {
+        static CovidDataData general;
         public static void Main(string[] args)
         {
+            RestServices.StartUpAPI();
+            getData();
             CreateHostBuilder(args).Build().Run();
         }
+
+        public static async void getData()
+        {
+            general = await RestServices.GetDataWorld();
+        }
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
---
+++ Covid-19-GalileiGalileo/Covid-19-GalileiGalileo/Services/RestServices.cs
@@ -0,0 +1,99 @@
+﻿using Covid_19_GalileiGalileo.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Covid_19_GalileiGalileo.Seriveces
+{
+    public static class RestServices
+    {
+        public static HttpClient client = new HttpClient();
+
+        public static void StartUpAPI()
+        {
+            client.DefaultRequestHeaders.Add("x-rapidapi-host", "coronavirus-monitor.p.rapidapi.com");
+            client.DefaultRequestHeaders.Add("x-rapidapi-key", "f4d025568cmsh12e79fdec8e33b1p174ff5jsn48a341aa1fc0");
+        }
+
+
+        public static async Task<CovidDataData> GetDataWorld()
+        {
+            CovidDataData Data = null;
+            string EndPoint = $"https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(EndPoint);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Data = JsonConvert.DeserializeObject<CovidDataData>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("\tERROR {0}", ex.Message);
+            }
+
+            return Data;
+        }
+
+
+        public static async Task<CovidDataData> GetLatestStatByCountry(string CountryName)
+        {
+
+            CovidDataData Data = null;
+            string EndPoint = $"https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=${CountryName}";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(EndPoint);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Data = JsonConvert.DeserializeObject<CovidDataData>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("\tERROR {0}", ex.Message);
+            }
+
+            return Data;
+        }
+
+
+
+        public static async Task<CovidDataData> GetDataHistoryByCountry(string CountryName)
+        {
+            CovidDataData Data = null;
+            string EndPoint = $"https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_particular_country.php?country={CountryName}";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(EndPoint);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Data = JsonConvert.DeserializeObject<CovidDataData>(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("\tERROR {0}", ex.Message);
+            }
+
+            return Data;
+        }
+
+
+
+
+
+    }
+}
