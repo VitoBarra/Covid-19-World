0130cbc6fd4750fde58f877e4f4f5fce90d948e2
Vito Barra <vb.vitobarra@gmail.com>
2020-04-06 13:11:27 +0200

    chart.js set up

--- Covid-19-GalileiGalileo/Covid-19-GalileiGalileo/Controllers/HomeController.cs
+++ Covid-19-GalileiGalileo/Covid-19-GalileiGalileo/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Covid_19_GalileiGalileo.Models;
 using Covid_19_GalileiGalileo.Services;
+using ChartJSCore.Models;
+using ChartJSCore.Helpers;
 
 namespace Covid_19_GalileiGalileo.Controllers
 {
@@ -23,10 +25,51 @@ namespace Covid_19_GalileiGalileo.Controllers
         {
             RestServices.StartUpAPI();
 
-            CovidData data = RestServices.GetDataWorld();
+            CovidData dataCovid = RestServices.GetDataWorld();
+
+
+            #region ChartCreation
+            Chart chart = new Chart();
+
+            chart.Type = Enums.ChartType.Line;
+
+            Data data = new Data();
+            data.Labels = new List<string>() { "January", "February", "March", "April", "May", "June", "July" };
+
+            LineDataset dataset = new LineDataset()
+            {
+                Label = "My First dataset",
+                Data = new List<double?> { 65, 59, 80, 81, 56, 55, 40 },
+                Fill = "false",
+                LineTension = 0.1,
+                BackgroundColor = ChartColor.FromRgba(75, 192, 192, 0.4),
+                BorderColor = ChartColor.FromRgb(75, 192, 192),
+                BorderCapStyle = "butt",
+                BorderDash = new List<int> { },
+                BorderDashOffset = 0.0,
+                BorderJoinStyle = "miter",
+                PointBorderColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
+                PointBackgroundColor = new List<ChartColor> { ChartColor.FromHexString("#ffffff") },
+                PointBorderWidth = new List<int> { 1 },
+                PointHoverRadius = new List<int> { 5 },
+                PointHoverBackgroundColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
+                PointHoverBorderColor = new List<ChartColor> { ChartColor.FromRgb(220, 220, 220) },
+                PointHoverBorderWidth = new List<int> { 2 },
+                PointRadius = new List<int> { 1 },
+                PointHitRadius = new List<int> { 10 },
+                SpanGaps = false
+            };
+
+            data.Datasets = new List<Dataset>();
+            data.Datasets.Add(dataset);
+
+            chart.Data = data;
+
+            ViewData["chart"] = chart;
+            #endregion
 
             if (data != null)
-                return View(data);
+                return View(dataCovid);
             else
                 return BadRequest();
         }
