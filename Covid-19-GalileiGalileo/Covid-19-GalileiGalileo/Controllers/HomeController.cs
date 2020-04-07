4f744aaf8d4519c9086d9a7b34644a7038c140ba
Vito Barra <vb.vitobarra@gmail.com>
2020-04-07 18:16:31 +0200

    new api and graph implementation

--- Covid-19-GalileiGalileo/Covid-19-GalileiGalileo/Controllers/HomeController.cs
+++ Covid-19-GalileiGalileo/Covid-19-GalileiGalileo/Controllers/HomeController.cs
@@ -21,55 +21,79 @@ namespace Covid_19_GalileiGalileo.Controllers
             _logger = logger;
         }
 
-        public  IActionResult Index()
+
+        public IActionResult Index()
         {
             RestServices.StartUpAPI();
 
-            CovidData dataCovid = RestServices.GetDataWorld();
+            List<CovidData> wordHistory = new List<CovidData>(RestServices.GetDataHistory());
+
+           
 
 
             #region ChartCreation
-            Chart chart = new Chart();
-
-            chart.Type = Enums.ChartType.Line;
-
-            Data data = new Data();
-            data.Labels = new List<string>() { "January", "February", "March", "April", "May", "June", "July" };
-
-            LineDataset dataset = new LineDataset()
+            Chart chart = new Chart()
             {
-                Label = "My First dataset",
-                Data = new List<double?> { 65, 59, 80, 81, 56, 55, 40 },
-                Fill = "false",
-                LineTension = 0.1,
-                BackgroundColor = ChartColor.FromRgba(75, 192, 192, 0.4),
-                BorderColor = ChartColor.FromRgb(75, 192, 192),
-                BorderCapStyle = "butt",
-                BorderDash = new List<int> { },
-                BorderDashOffset = 0.0,
-                BorderJoinStyle = "miter",
-                PointBorderColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
-                PointBackgroundColor = new List<ChartColor> { ChartColor.FromHexString("#ffffff") },
-                PointBorderWidth = new List<int> { 1 },
-                PointHoverRadius = new List<int> { 5 },
-                PointHoverBackgroundColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
-                PointHoverBorderColor = new List<ChartColor> { ChartColor.FromRgb(220, 220, 220) },
-                PointHoverBorderWidth = new List<int> { 2 },
-                PointRadius = new List<int> { 1 },
-                PointHitRadius = new List<int> { 10 },
-                SpanGaps = false
+                Type = Enums.ChartType.Line
             };
 
-            data.Datasets = new List<Dataset>();
-            data.Datasets.Add(dataset);
+            Data data = new Data()
+            {
+                Labels = new List<string>(),
+                Datasets = new List<Dataset>()
+                {
+                    new LineDataset()
+                    {
+                        Label = "casi positivi",
+                        Data = new List<double?>(),
+                        Fill = "true",
+                        LineTension = 0.1,
+                        BackgroundColor = ChartColor.FromRgba(75, 192, 192, 0.4),
+                        BorderColor = ChartColor.FromRgb(75, 192, 192),
+                        BorderCapStyle = "butt",
+                        BorderDash = new List<int> { },
+                        BorderDashOffset = 0.0,
+                        BorderJoinStyle = "miter",
+                        PointBorderColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
+                        PointBackgroundColor = new List<ChartColor> { ChartColor.FromHexString("#ffffff") },
+                        PointBorderWidth = new List<int> { 1 },
+                        PointHoverRadius = new List<int> { 5 },
+                        PointHoverBackgroundColor = new List<ChartColor> { ChartColor.FromRgb(75, 192, 192) },
+                        PointHoverBorderColor = new List<ChartColor> { ChartColor.FromRgb(220, 220, 220) },
+                        PointHoverBorderWidth = new List<int> { 2 },
+                        PointRadius = new List<int> { 1 },
+                        PointHitRadius = new List<int> { 10 },
+                        SpanGaps = false
+                    }
+                }
+            };
+
+
+
+            for (int i = 0; i < wordHistory.Count; i++)
+            {
+                if (i % 48 == 0 || i == wordHistory.Count - 1)
+                {
+                    data.Datasets[0].Data.Add(int.Parse(wordHistory[i].Cases.Total));
+                    if (i % 96 == 0 || i == wordHistory.Count - 1)
+                        data.Labels.Add(wordHistory[i].Time.ToString());
+                    else
+                        data.Labels.Add("");
+                }
+            }
+
+            data.Datasets[0].Data = data.Datasets[0].Data.Reverse().ToList();
+            data.Labels = data.Labels.Reverse().ToList();
 
             chart.Data = data;
 
             ViewData["chart"] = chart;
             #endregion
 
+
+
             if (data != null)
-                return View(dataCovid);
+                return View(wordHistory[0]);
             else
                 return BadRequest();
         }
