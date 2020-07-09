68b9b70f22876b733b419c3b538fab675a39c485
Vito Barra <vb.vitobarra@gmail.com>
2020-07-09 23:35:47 +0200

    Repo Cleaning

---
+++ Covid-19-World/Covid-19-World/Installers/MVCInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyPersonalStandardLibrary.AspNetCore.Installers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Westwind.AspNetCore.LiveReload;
+
+namespace Covid_World.Installers
+{
+    public class MVCInstaller : IInstaller
+    {
+        public void InstallService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();  
+            
+#if DEBUG
+            services.AddLiveReload();
+#endif
+        }
+    }
+}
